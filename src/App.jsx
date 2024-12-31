85e27d2ad57879d7ce12c726a489e3ad060b3144
Hai Pham <Hai.Pham@devsoft.vn>
2024-12-31 02:05:32 -0800

    intro component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+
 function App() {
-  return <div id="app"></div>;
+  const [start, setStart] = useState(false);
+
+  return (
+    <div id="app">
+      <div className="w-100 h-100 d-flex justify-content-center align-items-center">
+        {start ? (
+          <div className="session"></div>
+        ) : (
+          <div className="d-flex flex-column align-items-center intro">
+            <h3>RAG & HeyGen Video/Avatar Generated</h3>
+            <span>Avatar Streaming API + LiveKit</span>
+            <Button variant="primary" className="mt-4">
+              Start Demo
+            </Button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
