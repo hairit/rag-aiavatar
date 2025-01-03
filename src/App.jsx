a675e8291f986708503c3658418fb865fba93490
Hai Pham <Hai.Pham@devsoft.vn>
2025-01-03 02:32:45 -0800

    add streaming

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import { useState } from "react";
+import Streaming from "./components/streaming";
 import Button from "react-bootstrap/Button";
 
+// const heyGenUrl = "https://api.heygen.com";
+// const heyGenApiKey =
+//   "MDEwZDhiMDJjNDFmNDY0ZGJmYzMyOTgzNDQ3NDBhNGMtMTczNTQ1MjE3MQ==";
+
 function App() {
-  const [start, setStart] = useState(false);
+  const [streaming, setStreaming] = useState(false);
 
   return (
     <div id="app">
       <div className="w-100 h-100 d-flex justify-content-center align-items-center">
-        {start ? (
-          <div className="session"></div>
+        {streaming ? (
+          <Streaming exit={() => setStreaming(false)} />
         ) : (
           <div className="d-flex flex-column align-items-center intro">
             <h3>RAG & HeyGen Video/Avatar Generated</h3>
             <span>Avatar Streaming API + LiveKit</span>
-            <Button variant="primary" className="mt-4">
+            <Button
+              variant="primary"
+              className="mt-4"
+              onClick={() => setStreaming(true)}
+            >
               Start Demo
             </Button>
           </div>
---
+++ src/components/streaming.jsx
@@ -0,0 +1,3 @@
+export default function streaming() {
+  return <div>Test</div>;
+}
